3f68f5654f95d15076e6cff31c9edde22aa275d7
Christopher Andrews <candrews@middlebury.edu>
2019-04-22 17:37:20 -0400

    Conversion to bearer strategy

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-console */
 const express = require('express');
 const passport = require('passport');
-const CustomStrategy = require('passport-custom').Strategy;
+// const GoogleStrategy = require('passport-google-oauth2');
+// const CustomStrategy = require('passport-custom').Strategy;
+const BearerStrategy = require('passport-http-bearer').Strategy;
 const { OAuth2Client } = require('google-auth-library');
 const bodyParser = require('body-parser');
 
@@ -28,17 +30,44 @@ if (process.env.NODE_ENV === 'production') {
 // it into the application
 const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
-// Implement a simple passport custom strategy using the Google tokens as a
-// session
-passport.use('google-id-token', new CustomStrategy((request, done) => {
-  let token;
-  if (request.headers.authorization && request.headers.authorization.startsWith('Bearer ')) {
-    token = request.headers.authorization.slice(7);
-  } else {
-    done(null, false);
-    return;
-  }
+// // Implement a simple passport custom strategy using the Google tokens as a
+// // session
+// passport.use('google-id-token', new CustomStrategy((request, done) => {
+//   let token;
+//   if (request.headers.authorization && request.headers.authorization.startsWith('Bearer ')) {
+//     token = request.headers.authorization.slice(7);
+//   } else {
+//     done(null, false);
+//     return;
+//   }
 
+//   googleClient.verifyIdToken({
+//     idToken: token,
+//     audience: process.env.GOOGLE_CLIENT_ID,
+//   }).then((ticket) => {
+//     const payload = ticket.getPayload();
+//     // At this point you could create a user account from the payload if the user
+//     // doesn't have one and create a session for the user (or continue to use
+//     // the Google token as a session)
+//     done(null, payload);
+//   }).catch(() => done(null, false));
+// }));
+
+// passport.use(new GoogleStrategy({
+//   clientID: process.env.GOOGLE_CLIENT_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   callbackURL: process.env.GOOGLE_CALLBACK_URL,
+//   passReqToCallback: true,
+// },
+// (request, accessToken, refreshToken, profile, done) => {
+//   console.log(profile.id);
+//   // should be the user from the database that we looked up using the profile,
+//   // but we will just do this now
+//   return done(null, profile);
+// }));
+
+
+passport.use(new BearerStrategy((token, done) => {
   googleClient.verifyIdToken({
     idToken: token,
     audience: process.env.GOOGLE_CLIENT_ID,
@@ -56,7 +85,7 @@ app.use(passport.initialize());
 
 app.post(
   '/login',
-  passport.authenticate('google-id-token', { session: false }),
+  passport.authenticate('bearer', { session: false }),
   (request, response) => {
     let user = logins.get(request.user.email);
     if (user === undefined) {
